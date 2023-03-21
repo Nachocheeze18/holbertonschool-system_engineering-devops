3c063b73954a82fcc20fe86b8aec403ef117990c
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-20 19:49:13 -0700

    installing sql

---
+++ 0x04-mysql/0-installing.sql
@@ -0,0 +1,19 @@
+sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
+# Option 7
+# Option 1
+# Option 1
+# Option 4
+sudo apt update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
+sudo apt update
+sudo apt-cache policy mysql-server
+sudo apt install -yf mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*
+# Just Hit Enter When Asked For Password
+sudo mysql_secure_installation
+# Enter Password Twice
+# Option n
+# Option n
+# Option n
+# Option y
+sudo mysql -u root
